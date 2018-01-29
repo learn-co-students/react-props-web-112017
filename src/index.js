fc8ffbbd416d091a62ae90937e8aa8f71e9a67b2
KellyStamps <kellydsample@gmail.com>
2018-01-29 13:43:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // Code goes here
+// const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+// const title1 = 'Mad Max: Fury Road'
+// const madMaxPoster = "http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+//
+// ReactDOM.render(
+//   <MovieCard
+//     title=title1,
+//     poster=madMaxPoster,
+//     genres={madMaxGenres}
+//   />,
+//   document.getElementById('root')
+// );
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
