f6a55080817e1e8de03f71dcc2c6af757b2a12e7
Marvin Clerge <clergemarvin@gmail.com>
2018-01-29 12:34:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // Code goes here
+import React from 'React'
+import ReactDOM from 'react-dom'
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img src={this.props.poster} alt={this.props.title} />
+        <h2>{this.props.title}</h2>
+        <small>{this.props.genres.join(", ")}</small>
+      </div>
+    )
+  }
+}
+
+MovieCard.deafultProps = {
+  title: "Enter Title Here"
+}
+
+ReactDOM.render(
+  <MovieCard
+    title="My Awesome Movie"
+    poster="myawesomeposter.png"
+    genres=["I", "should", "have", "made", "a", "variable"]
+  />,
+  document.querySelector('#global')
+)
